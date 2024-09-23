e3f081b0a9da8fd71ee1768f9e7be23076077f5b
Pravin madavi <madavione@gmail.com>
2024-09-23 19:53:06 +0530

    User created by InMemoryDetailsManeger in userDetailsService

--- src/main/java/com/example/sbsecuritytest/Configs/MyConfig.java
+++ src/main/java/com/example/sbsecuritytest/Configs/MyConfig.java
@@ -4,7 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+//import org.springframework.security.config.http.SessionCreationPolicy;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,9 +22,20 @@ public class MyConfig {
     SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
                 (requests) -> requests.requestMatchers("/").permitAll().anyRequest().authenticated());
+
+        // for stateless session creation
+        // http.sessionManagement(session ->
+        // session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user1").password("{noop}mypassword").roles("user").build();
+        UserDetails user2 = User.withUsername("user2").password("{noop}mypassword2").roles("user").build();
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 
 }
