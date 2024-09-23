848b5a9eca02a3368370abe6496f8b54a0ea91e6
Pravin madavi <madavione@gmail.com>
2024-09-23 17:03:42 +0530

    Base commit

---
+++ src/main/java/com/example/sbsecuritytest/Controllers/AppController.java
@@ -0,0 +1,24 @@
+package com.example.sbsecuritytest.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class AppController {
+    @GetMapping("/")    //No authorization required
+    public String welcome() {
+        System.out.println("Application: Home page requested");
+        return "index";
+    }
+
+    
+    @GetMapping("/users")   //need to be authorized
+    public String userPage() {
+        System.out.println("Application: User page requested");
+        return "userpage";
+    }
+
+
+}
