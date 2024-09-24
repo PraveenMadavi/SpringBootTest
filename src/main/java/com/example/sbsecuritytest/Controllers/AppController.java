ef84ed36fee9411955da2692c150fd9dadbef766
Pravin madavi <madavione@gmail.com>
2024-09-24 17:12:19 +0530

    test pass

--- src/main/java/com/example/sbsecuritytest/Controllers/AppController.java
+++ src/main/java/com/example/sbsecuritytest/Controllers/AppController.java
@@ -3,6 +3,7 @@ package com.example.sbsecuritytest.Controllers;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 
@@ -19,6 +20,13 @@ public class AppController {
     public String userPage() {
         System.out.println("Application: User page requested");
         return "userpage";
+    }
+    @PreAuthorize("hasRole('admin')")
+    @GetMapping("/admin")   //need to be authorized
+    @ResponseBody
+    public String adminPage() {
+        System.out.println("Application: User page requested");
+        return "<h2>hello admin<h2> ";
     }
 
 
